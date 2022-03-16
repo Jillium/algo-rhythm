d7b54e5a57d9a0162eeb0e5d2bf4e02b32da288d
Jillium <86424914+Jillium@users.noreply.github.com>
2022-03-16 11:10:36 -0600

    arithmetic practice

---
+++ arithmetic/Unsolved/arithmetic.js
@@ -0,0 +1,22 @@
+// Fill in the body of each function with the code required
+
+var add = function(num1, num2) {
+    return num1 + num2
+};
+
+var subtract = function(num1, num2) {
+    return num1 - num2
+};
+
+var multiply = function(num1, num2) {
+    return num1 * num2
+};
+
+var divide = function(num1, num2) {
+    return num1 / num2
+};
+
+add(2,4);
+subtract(4,2);
+multiply(4,2);
+divide(12,3);
